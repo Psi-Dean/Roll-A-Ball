f93b786332c45714730467b45d39bc1104abd92e
dankr <dean.bingo@gmail.com>
2021-12-23 17:28:38 +0800

    modify move speed to 8

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -25,7 +25,7 @@ public class PlayerControl : MonoBehaviour
     {
         float MoveHori = Input.GetAxis("Horizontal");
         float MoveVert = Input.GetAxis("Vertical");
-        SphereBall.AddForce(new Vector3(MoveHori,0,MoveVert) * 3 );
+        SphereBall.AddForce(new Vector3(MoveHori,0,MoveVert) * 8 );
         if (timer > 0)
         {
             timer -= Time.deltaTime;
